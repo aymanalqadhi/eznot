6f2b9daf906bfa3dc75b34c6abb4f6e03f8a5792
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-05 01:17:56 +0300

    Formatted the config header file

--- include/config/config.h
+++ include/config/config.h
@@ -1,25 +1,26 @@
 #ifndef EZSRV_CONFIG_CONFIG_H
 #define EZSRV_CONFIG_CONFIG_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 /* Errors returned by `config_parse_argv' function */
-#define CONFIG_UNKNOWN_ERROR    -1
-#define CONFIG_MISSING_VALUE    -2
+#define CONFIG_UNKNOWN_ERROR -1
+#define CONFIG_MISSING_VALUE -2
 #define CONFIG_INVALID_AEGUMENT -3
-#define CONFIG_INVALID_VALUE    -4
-#define CONFIG_HELP_MESSAGE     -5
-#define CONFIG_VERSION_MESSAGE  -6
+#define CONFIG_INVALID_VALUE -4
+#define CONFIG_HELP_MESSAGE -5
+#define CONFIG_VERSION_MESSAGE -6
 
 /**
  * A structure to hold the application config
  */
-typedef struct app_config {
-  const char *executable_name;  /* The executing file name (argv[0]) */
+typedef struct app_config
+{
+	const char* executable_name; /* The executing file name (argv[0]) */
 
-  uint16_t listen_port;         /* The port the server listens on */
-  int ipv6;                     /* If set, IPv6 is used, default is IPv4 */
+	uint16_t listen_port; /* The port the server listens on */
+	int ipv6;			  /* If set, IPv6 is used, default is IPv4 */
 } app_config_t;
 
 /**
@@ -32,11 +33,11 @@ typedef struct app_config {
  *
  * @return 0 on success, negative value on failure
  */
-int config_parse_argv(
-    app_config_t *config,
-    int argc,
-    char ** argv,
-    bool handle_errors);
+int
+config_parse_argv(app_config_t* config,
+				  int argc,
+				  char** argv,
+				  bool handle_errors);
 
 /**
  * Gets the last error message
@@ -44,6 +45,7 @@ int config_parse_argv(
  * @return NULL if there was no error message, or
  *         a pointer to the error message string
  */
-const char* config_last_error();
+const char*
+config_last_error();
 
 #endif /* EZSRV_CONFIG_CONFIG_H */
