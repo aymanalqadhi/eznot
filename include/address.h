3914c1be2623fa40451a2aa97938f18e1283dddc
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-05 14:08:09 +0300

    Add addresses utility functions

---
+++ include/address.h
@@ -0,0 +1,9 @@
+#ifndef EZNOT_ADDRESS_H
+#define EZNOT_ADDRESS_H
+
+#define EZNOT_IPV6_ANY      "::"
+#define EZNOT_IPV4_ANY      "0.0.0.0"
+#define EZNOT_IPV6_LOOPBACK "::"
+#define EZNOT_IPV4_LOOPBACK "127.0.0.1"
+
+#endif /* end of include guard: EZNOT_ADDRESS_H */
