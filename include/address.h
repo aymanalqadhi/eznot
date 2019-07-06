872bd770e32a3e3a939add4489e5afc5f8aa11ee
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-06 17:50:43 +0300

    Add utility address conversion union

--- include/address.h
+++ include/address.h
@@ -1,9 +1,18 @@
 #ifndef EZNOT_ADDRESS_H
 #define EZNOT_ADDRESS_H
 
+#include "uv.h"
+
 #define EZNOT_IPV6_ANY      "::"
 #define EZNOT_IPV4_ANY      "0.0.0.0"
 #define EZNOT_IPV6_LOOPBACK "::"
 #define EZNOT_IPV4_LOOPBACK "127.0.0.1"
+
+union ip_address {
+	struct sockaddr addr;
+	struct sockaddr_in addrv4;
+	struct sockaddr_in6 addrv6;
+	struct sockaddr_storage storage;
+};
 
 #endif /* end of include guard: EZNOT_ADDRESS_H */
