d1d8438c7d2a60953e11aa71eca27beb22a243d3
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-07 16:28:23 +0300

    Start implementing messaging logic

--- src/callbacks.c
+++ src/callbacks.c
@@ -1,6 +1,9 @@
+#include "address.h"
 #include "callbacks.h"
 #include "memory.h"
 #include "request_message.h"
+#include "subscribers.h"
+#include "publishers.h"
 
 #include "log.c"
 #include "uv.h"
@@ -9,16 +12,21 @@
 
 /******************************************************************************/
 
+static int
+get_ip_str(const struct sockaddr* sa, char* s, size_t maxlen);
+
+/******************************************************************************/
+
 void
 eznot_on_recv(uv_udp_t* handle,
-              ssize_t nread,
-              const uv_buf_t* buff,
-              const struct sockaddr* from,
-              unsigned int flags)
+			  ssize_t nread,
+			  const uv_buf_t* buff,
+			  const struct sockaddr* from,
+			  unsigned int flags)
 {
 	/* TODO:
-	 * 1. Write a real implementation
-	 * 2. Handle invalid requests
+	 * 1. Write a real implementation (20%)
+	 * 2. Handle invalid requests (50%)
 	 * 3. Replay to requests
 	 */
 
@@ -27,23 +35,28 @@ eznot_on_recv(uv_udp_t* handle,
 	} else if (nread >= REQUEST_MESSAGE_TOTAL_SIZE) {
 		request_message_t req;
 		int ret = eznot_decode_request_message(
-		    buff->base, REQUEST_MESSAGE_TOTAL_SIZE, &req);
+			buff->base, REQUEST_MESSAGE_TOTAL_SIZE, &req);
 
-		if (ret == REQUEST_MESSAGE_TOTAL_SIZE) {
-			log_debug("------------------------------------");
-			log_debug("Got a message!");
-			log_debug("------------------------------------");
-			log_debug("msg.header.message_type = %d", req.header.message_type);
-			log_debug("msg.header.flags        = %d", req.header.flags);
-			log_debug("msg.header.__hchecksum  = %d", req.header.__hchecksum);
-			log_debug("msg.header.__pchecksum  = %d", req.header.__pchecksum);
-			log_debug("------------------------------------");
-			log_debug("msg.payload.tags = %s", req.payload.tags);
-			log_debug("msg.payload.data = %s", req.payload.data);
-			log_debug("------------------------------------");
+		if (ret != REQUEST_MESSAGE_TOTAL_SIZE) {
+			/* TODO: Handle invalid request */
+			goto clean_exit;
+		}
+
+		char fromaddr[INET6_ADDRSTRLEN];
+		if (get_ip_str(from, fromaddr, INET6_ADDRSTRLEN) != 0) {
+			/* TODO: Handle error */
+			goto clean_exit;
+		}
+
+		switch (req.header.message_type) {
+		case PUBLISH_MESSAGE:
+			goto clean_exit;
+		case SUBSCRIBE_MESSAGE:
+			goto clean_exit;
 		}
 	}
 
+clean_exit:
 	free(buff->base);
 }
 
@@ -58,6 +71,28 @@ eznot_on_alloc(uv_handle_t* client, size_t suggested_size, uv_buf_t* buff)
 	buff->len = suggested_size;
 
 	log_debug("Allocated %lu bytes in %p.", suggested_size, buff->base);
+}
+
+/******************************************************************************/
+
+static int
+get_ip_str(const struct sockaddr* sa, char* s, size_t maxlen)
+{
+	switch (sa->sa_family) {
+	case AF_INET:
+		inet_ntop(AF_INET, &(((struct sockaddr_in*)sa)->sin_addr), s, maxlen);
+		break;
+
+	case AF_INET6:
+		inet_ntop(
+			AF_INET6, &(((struct sockaddr_in6*)sa)->sin6_addr), s, maxlen);
+		break;
+
+	default:
+		return -1;
+	}
+
+	return 0;
 }
 
 /******************************************************************************/
