39a4d96c3aaad9b1886d4a44b521f93a140672ba
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-09 01:59:35 +0300

    Use the new services init and destroy calls

--- src/server.c
+++ src/server.c
@@ -1,28 +1,22 @@
-#include "server.h"
 #include "address.h"
 #include "callbacks.h"
 #include "publishers.h"
+#include "server.h"
 #include "subscribers.h"
 
-#include "jobs_runner.h"
 #include "jobs/send_not.h"
+#include "jobs_runner.h"
 
 #include "log.h"
 #include "uv.h"
 
-#include <stdlib.h>
 #include <assert.h>
-
-/*
- * TODO:
- * Move this to config
- */
-#define THREAD_POOL_SIZE 8
+#include <stdlib.h>
 
 /******************************************************************************/
 
 int
-init_eznot_server(eznot_server_t *server, app_config_t *config)
+init_eznot_server(eznot_server_t* server, app_config_t* config)
 {
 	log_trace("init_eznot_server()");
 
@@ -30,8 +24,8 @@ init_eznot_server(eznot_server_t *server, app_config_t *config)
 		return -1;
 	}
 
-	server->config      = config;
-	server->loop        = uv_default_loop();
+	server->config = config;
+	server->loop = uv_default_loop();
 	server->handle.data = server;
 
 	int rc;
@@ -40,15 +34,16 @@ init_eznot_server(eznot_server_t *server, app_config_t *config)
 		return -1;
 	} else {
 		log_debug("Reading trusted publishers...");
-		rc = eznot_load_publishers(config->trusted_publishers_file);
+		rc = eznot_init_publishers_hashtable(config);
 		if (rc != 0) {
 			log_error("Could not load publishers!");
 			return -1;
 		}
 	}
 
-	log_debug("Initializig thread pool with %d threads...", THREAD_POOL_SIZE);
-	if (eznot_init_jobs_runner(THREAD_POOL_SIZE) != 0) {
+	log_debug("Initializig thread pool with %d threads...",
+			  config->threads_count);
+	if (eznot_init_jobs_runner(config) != 0) {
 		log_error("Could initialize thread pool!");
 		return -1;
 	}
@@ -74,32 +69,31 @@ init_eznot_server(eznot_server_t *server, app_config_t *config)
 /******************************************************************************/
 
 int
-start_eznot_server(const eznot_server_t *server)
+start_eznot_server(const eznot_server_t* server)
 {
 	log_trace("start_eznot_server()");
 	int rc;
 
-	if (server->config->ipv6) {
+	if (server->config->use_ipv6) {
 		struct sockaddr_in6 addr;
 
 		rc = uv_ip6_addr(EZNOT_IPV6_ANY, server->config->listen_port, &addr);
 		UV_CHECK(rc, "uv_ip6_addr");
-		rc = uv_udp_bind((uv_udp_t *)&server->handle,
-						(struct sockaddr *)&addr, 0);
+		rc =
+			uv_udp_bind((uv_udp_t*)&server->handle, (struct sockaddr*)&addr, 0);
 		UV_CHECK(rc, "uv_udp_bind");
 	} else {
 		struct sockaddr_in addr;
 
 		rc = uv_ip4_addr(EZNOT_IPV4_ANY, server->config->listen_port, &addr);
 		UV_CHECK(rc, "uv_ip4_addr");
-		rc = uv_udp_bind((uv_udp_t *)&server->handle,
-						(struct sockaddr *)&addr, 0);
+		rc =
+			uv_udp_bind((uv_udp_t*)&server->handle, (struct sockaddr*)&addr, 0);
 		UV_CHECK(rc, "uv_udp_bind");
 	}
 
-	rc = uv_udp_recv_start((uv_udp_t *)&server->handle,
-						   &eznot_on_alloc,
-						   &eznot_on_recv);
+	rc = uv_udp_recv_start(
+		(uv_udp_t*)&server->handle, &eznot_on_alloc, &eznot_on_recv);
 	UV_CHECK(rc, "uv_udp_recv_start");
 	rc = uv_run(server->loop, UV_RUN_DEFAULT);
 	UV_CHECK(rc, "uv_run");
@@ -110,7 +104,7 @@ start_eznot_server(const eznot_server_t *server)
 /******************************************************************************/
 
 void
-free_eznot_server(eznot_server_t *server)
+free_eznot_server(eznot_server_t* server)
 {
 	log_trace("free_eznot_server()");
 
