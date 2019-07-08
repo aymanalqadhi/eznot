da67de7e0d4b217e7b20c39f85d6baef06e1d2ef
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-09 01:58:40 +0300

    Use the service style for the init and destroy functions

--- include/jobs_runner.h
+++ include/jobs_runner.h
@@ -1,13 +1,15 @@
 #ifndef EZNOT_JOBS_RUNNER_H
 #define EZNOT_JOBS_RUNNER_H
 
-#include <stddef.h>
+#include "config/config.h"
 
-int
-eznot_init_jobs_runner(size_t threads_count);
+#include <stddef.h>
 
 void
 eznot_enqueue_job(void (*job_func)(void*), void*);
+
+int
+eznot_init_jobs_runner(const app_config_t *conf);
 
 void
 eznot_destroy_jobs_runner();
--- src/jobs_runner.c
+++ src/jobs_runner.c
@@ -1,4 +1,5 @@
 #include "jobs_runner.h"
+#include "config/config.h"
 
 #include "thpool.h"
 #include "log.h"
@@ -14,8 +15,16 @@ static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 /******************************************************************************/
 
+void
+eznot_enqueue_job(void (*job_func)(void*), void* data)
+{
+	thpool_add_work(thpool, job_func, data);
+}
+
+/******************************************************************************/
+
 int
-eznot_init_jobs_runner(size_t threads_count)
+eznot_init_jobs_runner(const app_config_t *conf)
 {
 	log_trace("eznot_init_jobs_runner()");
 
@@ -26,21 +35,13 @@ eznot_init_jobs_runner(size_t threads_count)
 
 	pthread_mutex_lock(&lock);
 
-	thpool = thpool_init(threads_count);
+	thpool = thpool_init(conf->threads_count);
 	inited = true;
 	destroyed = false;
 
 	pthread_mutex_unlock(&lock);
 
 	return 0;
-}
-
-/******************************************************************************/
-
-void
-eznot_enqueue_job(void (*job_func)(void*), void* data)
-{
-	thpool_add_work(thpool, job_func, data);
 }
 
 /******************************************************************************/
